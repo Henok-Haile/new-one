cd3768028adb8dae17492a0ce3823fa0d8f665a5
Henok-Haile <henokhaile53@gmail.com>
2025-01-27 10:00:45 +0300

    second

---
+++ config.js
@@ -0,0 +1,5 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://root:l0mxghZWLGExLv6Y@books-store-mern.cewxv.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN';
+
+// export const mongoDBURL = 'mongodb+srv://root:l0mxghZWLGExLv6Y@books-store-mern.cewxv.mongodb.net/'
