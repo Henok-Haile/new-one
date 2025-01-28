274b282f37a82e43f2cd2b1f26aa5214e95b9cfd
Henok-Haile <henokhaile53@gmail.com>
2025-01-28 11:27:09 +0300

    task

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 // app.use(cors());
 app.use(
     cors({
-        origin: 'http://localhost:5173',
+        origin: 'https://fullstack-web-developmnet-na-amal-duh6.vercel.app/',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type'],
     })
