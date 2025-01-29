27f12d62f3c6fd665b5bad5cef303e8405484989
Henok-Haile <henokhaile53@gmail.com>
2025-01-29 11:10:16 +0300

    test

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -13,7 +13,7 @@ export const protect = async (req, resizeBy, next) => {
             token = req.headers.authorization.split(" ")[1]; // Get the token
 
 
-             req.headers.authorization= "Bearer "+token
+            //  req.headers.authorization= "Bearer "+token
 
       
 
